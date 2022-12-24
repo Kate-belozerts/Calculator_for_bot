7e82ea115d6aff28a88bd20a2f3b2174f324c7c9
Kate Belozertseva <katebelozertseva@gmail.com>
2022-12-24 19:49:34 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# operations = {'+': lambda x, y: x + y,
+#     '-': lambda x, y: x - y,
+#     '*': lambda x, y: x * y,
+#     '/': lambda x, y: x / y}
+#
+#
+# op = '+'
+# n1, n2 = 25, 6
+#
+#
+# print(operations[op](n1, n2))
