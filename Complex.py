e109f79e1a88c6783de0f82381c043b3be073c8e
Kate Belozertseva <katebelozertseva@gmail.com>
2023-01-06 20:36:17 +0300

    Corrected calculator-bot

--- Complex.py
+++ Complex.py
@@ -2,22 +2,61 @@ import Logger
 
 
 def to_complex(operation):
-    # result = complex(operation)
-    result = eval(operation)  # eval("2 * 0.033e-3 + 1j * 0.12e-3")
+    operation = operation.replace(' ', '')
+    z1 = ''
+    z2 = ''
+    if '=' in operation:
+        a = operation.index('=')
+        b = operation[a:].index('z')
+        size = operation[a+1:b]
+        for i in range(len(size)):
+            z1 += size[i]
+        sign = operation[b]
+        z1 = complex(z1)
+
+        size = operation[b+3:]
+        for j in range(len(size)):
+            z2 += size[j]
+        z2 = complex(z2)
+
+    else:
+        a = operation.index('j')
+        size = operation[:a+1]
+        for i in range(len(size)):
+            z1 += size[i]
+        z1 = complex(z1)
+        sign = operation[a+1]
+
+        size = operation[a+2:]
+        for j in range(len(size)):
+            z2 += size[j]
+        z2 = complex(z2)
+
+    if sign == '+':
+        result = z1 + z2
+    if sign == '-':
+        result = z1 - z2
+    if sign == '/':
+        result = z1 / z2
+    if sign == '*':
+        result = z1 * z2
+
     Logger.logger(result, f'complex from {operation}')
-    print(result)
     return result
 
 
-# to_complex("1.5+7j-8+7")
-# print(complex(1.5+7j + 5))
-# print(int((1.5+7j).real))
+# o = 'z = 2+5j + z = 3 - 3j'
+# b = '2 + 5j - 6 - 3j'
+# to_complex(b)
 
 
-#i = -1**0.5 --- корень из -1
+
+
+
 #a, b - float
 #if b/a == 0 ----- comlex  -> float
 # a - действительная часть
 # b - мнимая часть с i
-# a + bi --- единое число !!!
-
+# a + bi --- единое число
+#i = -1**0.5 --- корень из -1
+# print(int((1.5+7j).real))
